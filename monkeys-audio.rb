c3aef1d56191d823f61a85e1f3ff626a792bd9ec
Fernando Tarlá Cardoso Lemos <fernandotcl@gmail.com>
2013-01-02 15:28:19 -0200

    Releases will also depend on running autogen.sh.
    GitHub no longer supports manually uploaded sources. This means
    we'll have to run autogen.sh both for HEAD and releases.

--- monkeys-audio.rb
+++ monkeys-audio.rb
@@ -4,11 +4,9 @@ class MonkeysAudio < Formula
   homepage 'https://github.com/fernandotcl/monkeys-audio'
   head 'https://github.com/fernandotcl/monkeys-audio.git'
 
-  # autoconf, automake and libtool are only needed for HEAD
   depends_on 'autoconf' => :build
   depends_on 'automake' => :build
   depends_on 'libtool' => :build
-
   depends_on 'yasm' => :build
 
   def install
