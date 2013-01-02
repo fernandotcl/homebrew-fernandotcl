8c1f50a80d2db568e0651f63f4f49c32f6a868b2
Fernando Tarlá Cardoso Lemos <fernandotcl@gmail.com>
2013-01-02 19:54:08 -0200

    Added a release URL for monkeys-audio (version 3.99-u4-b6).

--- monkeys-audio.rb
+++ monkeys-audio.rb
@@ -3,6 +3,8 @@ require 'formula'
 class MonkeysAudio < Formula
   homepage 'https://github.com/fernandotcl/monkeys-audio'
   head 'https://github.com/fernandotcl/monkeys-audio.git'
+  url 'https://github.com/fernandotcl/monkeys-audio/archive/release-3.99-u4-b6.tar.gz'
+  sha1 'bda0a6744c3da22a8b9949dad0e1375269bfb9d7'
 
   depends_on 'autoconf' => :build
   depends_on 'automake' => :build
