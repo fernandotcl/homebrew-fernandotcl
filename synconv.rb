8e4b0617870f8de28195e81152a63456a3afc864
Fernando Tarlá Cardoso Lemos <fernandotcl@gmail.com>
2013-02-10 22:14:52 -0200

    New Cocoa-based synconv, no need for libpipeline

--- synconv.rb
+++ synconv.rb
@@ -3,13 +3,9 @@ require 'formula'
 class Synconv < Formula
   homepage 'https://github.com/fernandotcl/synconv'
   head 'https://github.com/fernandotcl/synconv.git'
-  url 'https://github.com/downloads/fernandotcl/synconv/synconv-1.1.2.tar.gz'
-  sha1 '0ad68f3214425765a7f2f9ee86bee68a911ebfe8'
 
   depends_on 'cmake' => :build
   depends_on 'pkg-config' => :build
-  depends_on 'boost'
-  depends_on 'libpipeline'
   depends_on 'taglib'
   depends_on 'flac' => :optional
   depends_on 'lame' => :optional
