4e8c90ce770a224e78de227ba810b9da282d5549
Fernando Tarlá Cardoso Lemos <fernandotcl@gmail.com>
2013-02-15 23:24:52 -0200

    monkeys-audio now uses CMake

--- monkeys-audio.rb
+++ monkeys-audio.rb
@@ -3,20 +3,13 @@ require 'formula'
 class MonkeysAudio < Formula
   homepage 'https://github.com/fernandotcl/monkeys-audio'
   head 'https://github.com/fernandotcl/monkeys-audio.git'
-  url 'https://github.com/fernandotcl/monkeys-audio/archive/release-3.99-u4-b6.tar.gz'
-  version '3.99-u4-b6'
-  sha1 'bda0a6744c3da22a8b9949dad0e1375269bfb9d7'
 
-  depends_on 'autoconf' => :build
-  depends_on 'automake' => :build
-  depends_on 'libtool' => :build
+  depends_on 'cmake' => :build
   depends_on 'yasm' => :build
 
   def install
-    system "./autogen.sh"
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--prefix=#{prefix}"
+    system "cmake", ".", *std_cmake_args
     system "make install"
+    man1.install "man/mac.1"
   end
 end
