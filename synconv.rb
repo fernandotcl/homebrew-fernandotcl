fcfddea12d37e245bc81b0ee956c9669aa0407f0
Fernando Tarlá Cardoso Lemos <fernandotcl@gmail.com>
2013-03-25 21:15:44 -0300

    Point to synconv 2.0.0

--- synconv.rb
+++ synconv.rb
@@ -3,6 +3,8 @@ require 'formula'
 class Synconv < Formula
   homepage 'https://github.com/fernandotcl/synconv'
   head 'https://github.com/fernandotcl/synconv.git'
+  url 'https://github.com/fernandotcl/synconv/archive/release-2.0.0.tar.gz'
+  sha1 'f6ae25d35c863748ec92fa4f88737fb898f40ccb'
 
   depends_on 'cmake' => :build
   depends_on 'pkg-config' => :build
