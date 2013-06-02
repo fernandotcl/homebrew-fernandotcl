5a28ff35618721a30397d0141dd31c042d0ea9bc
Fernando Tarlá Cardoso Lemos <fernandotcl@gmail.com>
2013-06-02 19:21:48 -0300

    Point to monkeys-audio 3.99.5

--- monkeys-audio.rb
+++ monkeys-audio.rb
@@ -3,6 +3,8 @@ require 'formula'
 class MonkeysAudio < Formula
   homepage 'https://github.com/fernandotcl/monkeys-audio'
   head 'https://github.com/fernandotcl/monkeys-audio.git'
+  url 'https://github.com/fernandotcl/monkeys-audio/archive/release-3.99.5.tar.gz'
+  sha1 '09d4067b38778b7b3ff3f7a183387fca9a821965'
 
   depends_on 'cmake' => :build
   depends_on 'yasm' => :build
