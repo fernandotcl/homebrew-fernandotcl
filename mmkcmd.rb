91cb2847da0063dcf3c0023b42cd1409632bb0a1
Fernando Tarlá Cardoso Lemos <fernandotcl@gmail.com>
2014-02-15 18:51:18 +0100

    Point to mmkcmd 1.0.0

--- mmkcmd.rb
+++ mmkcmd.rb
@@ -3,6 +3,8 @@ require 'formula'
 class Mmkcmd < Formula
   homepage 'https://github.com/fernandotcl/mmkcmd'
   head 'https://github.com/fernandotcl/mmkcmd.git'
+  url 'https://github.com/fernandotcl/mmkcmd/archive/release-1.0.0.tar.gz'
+  sha1 '58b29911e6bfc1f809a8272720332fa7552cc3af'
 
   depends_on 'cmake' => :build
 
